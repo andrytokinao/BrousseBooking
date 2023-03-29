fa66cbc79f1af62679aae3d14c767f42f3eba01d
andrytokinao <tokinao.mg@gmail.com>
2023-03-29 04:10:37 +0300

    initial commit

---
+++ src/main/java/com/kinga/BrousseBooking/BrousseBookingApplication.java
@@ -0,0 +1,13 @@
+package com.kinga.BrousseBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrousseBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrousseBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinga/BrousseBooking/BrousseBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinga.BrousseBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrousseBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
